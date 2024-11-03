3d2d8ffd61fa000094b1b8b82000013651642215
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2024-11-03 21:27:25 +0100

    about added

--- flask_hippocooking/homepage/main/routes.py
+++ flask_hippocooking/homepage/main/routes.py
@@ -4,3 +4,12 @@ from . import main
 @main.route('/')
 def index():
      return render_template('index.html', title='Blog')
+
+# @main.route('/favicon.ico')
+# def favicon():
+#     return send_from_directory(os.path.join(current_app.root_path, 'static'), 'resources/img/favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+@main.route('/about')
+def about():
+    return render_template('about.html', title='Über uns')
