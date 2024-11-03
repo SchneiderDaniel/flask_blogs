142120811d3d132529743a96ae6186afc8e1c6c2
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2024-11-03 21:28:38 +0100

    tempalte structure

--- flask_hippocooking/homepage/main/routes.py
+++ flask_hippocooking/homepage/main/routes.py
@@ -3,7 +3,7 @@ from . import main
 
 @main.route('/')
 def index():
-     return render_template('index.html', title='Blog')
+     return render_template('main/index.html', title='Blog')
 
 # @main.route('/favicon.ico')
 # def favicon():
@@ -12,4 +12,4 @@ def index():
 
 @main.route('/about')
 def about():
-    return render_template('about.html', title='Über uns')
+    return render_template('main/about.html', title='Über uns')
