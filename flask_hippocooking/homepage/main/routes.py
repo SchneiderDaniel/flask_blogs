af49adb76834ed5c9b1e60e9d486014bbf5bf856
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2024-11-03 21:34:16 +0100

    favicon added

--- flask_hippocooking/homepage/main/routes.py
+++ flask_hippocooking/homepage/main/routes.py
@@ -1,13 +1,14 @@
-from flask import render_template
+from flask import render_template, send_from_directory, current_app
 from . import main
+import os
 
 @main.route('/')
 def index():
      return render_template('main/index.html', title='Blog')
 
-# @main.route('/favicon.ico')
-# def favicon():
-#     return send_from_directory(os.path.join(current_app.root_path, 'static'), 'resources/img/favicon.ico', mimetype='image/vnd.microsoft.icon')
+@main.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(current_app.root_path, 'static'), 'resources/icons/favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @main.route('/about')
