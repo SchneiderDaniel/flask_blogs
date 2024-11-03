8af5fd3498752be0e704f0b679addbaa7b148e31
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2024-11-03 14:42:46 +0100

    added local python

--- flask_hippocooking/run.py
+++ flask_hippocooking/run.py
@@ -2,5 +2,5 @@ from homepage import create_app
 
 app = create_app()
 
-# if __name__ == "__main__":
-#     app.run(debug=True, threaded=True)
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
