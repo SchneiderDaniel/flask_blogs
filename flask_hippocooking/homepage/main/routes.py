ac42edf4f17239d1bf2e0b52e63d4bc661c7510c
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2024-11-03 21:03:40 +0100

    first base

--- flask_hippocooking/homepage/main/routes.py
+++ flask_hippocooking/homepage/main/routes.py
@@ -1,7 +1,6 @@
-# from flask import render_template, request, redirect, url_for, send_from_directory, flash, Blueprint, current_app
 from flask import render_template
 from . import main
 
 @main.route('/')
 def index():
-     return render_template('index.html')
+     return render_template('index.html', title='Blog')
