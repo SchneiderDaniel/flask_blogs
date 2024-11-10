f3bb928d6dd28c4c298950cefad8be4395e10e69
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2024-11-10 20:29:54 +0100

    pychache

--- flask_hippocooking/homepage/errors/handlers.py
+++ flask_hippocooking/homepage/errors/handlers.py
@@ -3,17 +3,20 @@ from flask import Blueprint, render_template
 
 errors = Blueprint('errors', __name__)
 
+data = {
+        "titel": "Error",
+}
 
 @errors.app_errorhandler(404)
 def error404(error):
-    return render_template('errors/404.html'), 404
+    return render_template('errors/404.html', translations=data, locale_id="en"), 404
 
 
 @errors.app_errorhandler(403)
 def error403(error):
-    return render_template('errors/403.html'), 403
+    return render_template('errors/403.html', translations=data, locale_id="en"), 403
 
 
 @errors.app_errorhandler(500)
 def error500(error):
-    return render_template('errors/404.html'), 500
+    return render_template('errors/404.html', translations=data, locale_id="en"), 500
