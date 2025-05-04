4348732036984584c5348eae99a756640a26507b
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2025-05-04 15:33:35 +0200

    script

---
+++ renew_cert.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Define log directory and clean up old logs
+LOGDIR="/var/log"
+LOGPREFIX="certbot_renew_"
+find "$LOGDIR" -name "${LOGPREFIX}*.log" -type f -delete
+
+# Create new log file with current timestamp
+LOGFILE="${LOGDIR}/${LOGPREFIX}$(date '+%Y%m%d_%H%M%S').log"
+
+{
+    echo "Starting certificate renewal at $(date)"
+
+    # Execute certbot with automatic 'yes' to renew
+    docker exec certbot /bin/sh -c "
+        echo '2' | certbot certonly --webroot -w /var/www/certbot -d hippocooking.com -d www.hippocooking.com
+    "
+
+    echo "Restarting Docker services..."
+    docker-compose restart
+
+    echo "Certificate renewal completed at $(date)"
+} &> "$LOGFILE"
